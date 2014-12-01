52a1382e038a2834e20215128d3b62ac89f2a971
sagiegurari <sagiegurari@gmail.com>
2014-12-01 22:08:24 +0200

    adding support for continues integration - code coverage

--- test/spec/spider-loader-spec.js
+++ test/spec/spider-loader-spec.js
@@ -8,6 +8,26 @@ var assert = chai.assert;
 var spiderLoader = require('../../lib/spider-loader');
 
 describe('Spider Loader Tests', function () {
+    describe('runSpiderScript2JS Tests', function () {
+        it('runSpiderScript2JS valid', function () {
+            var spiderFile = path.resolve(__dirname, '../helpers/test.spider');
+            var js = spiderLoader.runSpiderScript2JS(spiderFile);
+
+            assert.isString(js);
+            assert.isTrue(js.length > 0);
+        });
+
+        it('runSpiderScript2JS error', function () {
+            var spiderFile = path.resolve(__dirname, '../helpers/error.spider');
+            try {
+                var js = spiderLoader.runSpiderScript2JS(spiderFile);
+                assert.notOk(js);
+            } catch (error) {
+                assert.isDefined(error);
+            }
+        });
+    });
+
     describe('loadSpiderScriptSync Tests', function () {
         it('loadSpiderScriptSync valid', function () {
             var spiderFile = path.resolve(__dirname, '../helpers/test.spider');
